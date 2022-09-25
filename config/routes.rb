67d279f6d6b214b5a81b8ba8a2f4edb115afdeca
nozomi tanaka <nozomitanaka@nozomi-no-MacBook-Air.local>
2022-09-25 11:12:42 +0900

    フラッシュ機能の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#top'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
