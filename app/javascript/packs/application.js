ca77605e4f35426acc6cb4e623bffc16cd20e90a
nozomi tanaka <nozomitanaka@nozomi-no-MacBook-Air.local>
2022-10-01 12:20:24 +0900

    ハンバーガーメニュークリック実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails, { $ } from "@rails/ujs"
+import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
@@ -11,5 +11,16 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+require('jquery')
 
+$(function(){
+  $('.notice').fadeOut(2000);
+});
 
+$(function(){
+
+  $(".hamburger-menu").on("click", function(){
+    $(this).nextAll('.menu-list').slideToggle("fast");
+  });
+
+});
