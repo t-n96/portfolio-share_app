03b24c8953b866567015d52bb5821e37927d2bec
nozomi tanaka <nozomitanaka@nozomi-no-MacBook-Air.local>
2022-10-02 11:17:22 +0900

    ゲストログイン実装

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,6 +24,12 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
   def after_sign_in_path_for(resource)
     flash[:notice] = "ログインに成功しました。"
     root_path
@@ -33,4 +39,5 @@ class Users::SessionsController < Devise::SessionsController
     flash[:notice] = "ログアウトしました。"
     root_path
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :validatable
 
   mount_uploader :avatar, AvatarUploader
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.name = 'ゲスト'
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
