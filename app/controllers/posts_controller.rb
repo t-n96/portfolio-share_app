4d7eb6c542795024f0024cd74bfe537906d1683e
nozomi tanaka <nozomitanaka@nozomi-no-MacBook-Air.local>
2022-10-08 09:40:56 +0900

    確認画面実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,13 +8,22 @@ class PostsController < ApplicationController
 
   def confirm
     @post = Post.new(post_params)
+    if @post.invalid?
+      redirect_to new_post_path, notice: '入力に誤りがあります。'
+    end
   end
 
   def create
     @post = Post.new(post_params)
+    if params[:back] || !@post.save
+      render :new and return
+    end
+    flash[:notice] = "投稿完了しました。"
+    redirect_to root_path
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
