8671bca2b05ad8529c3a5b20c7ecaad00f12861c
nozomi tanaka <nozomitanaka@nozomi-no-MacBook-Air.local>
2022-11-18 06:30:41 +0900

    テストデータ作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   mount_uploader :post_image, PostImageUploader
   validates :title, :introduction, :post_image, presence: true
   validates :title, length: { in: 1..15 }
-  validates :introduction, length: { in: 1..75 }
+  validates :introduction, length: { in: 1..105 }
 
   belongs_to :user
 
