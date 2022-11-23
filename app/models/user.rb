6d0feaf77cf3653a6c8ed099de9b9677e8448964
nozomi tanaka <nozomitanaka@nozomi-no-MacBook-Air.local>
2022-11-23 10:04:27 +0900

    いいねcss変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+  validates :name, length: { in: 1..8 }
 
   mount_uploader :avatar, AvatarUploader
 
