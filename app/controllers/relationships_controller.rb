8a38f0eb54d618569caaa8e904bb01cf6c320b9e
nozomi tanaka <nozomitanaka@nozomi-no-MacBook-Air.local>
2023-01-03 12:04:33 +0900

    フォロー機能のボタンcss修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,11 +2,13 @@ class RelationshipsController < ApplicationController
 
   def create
     current_user.follow(params[:user_id])
+    flash[:notice] = "フォローしました。"
     redirect_to request.referer
   end
 
   def destroy
     current_user.unfollow(params[:user_id])
+    flash[:notice] = "フォローを外しました。"
     redirect_to request.referer
   end
 
